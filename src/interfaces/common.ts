0804ae19d483fa300d4883c724bed5757c0a36e9
kukuhsetiawan13 <study.research153@passfwd.com>
2025-02-08 22:52:36 +0700

    add common interface

---
+++ src/interfaces/common.ts
@@ -0,0 +1,9 @@
+export type ArrayOfStringsType = string[];
+
+export type ArrayOfNumbersType = number[];
+
+export type BasicMapType = Map<string, object>;
+
+export interface BasicObjectInterface {
+  id: string;
+}
